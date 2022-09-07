37a2a96b35a4090b64ee8c98daa6631b01c66275
Caro-ag90 <cacunag@unicaragena.edu.co>
2022-09-07 08:25:26 -0500

    update

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,5 +1,11 @@
 package com.hibernate.introduction;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.introduction.modelo.Mascota;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //Crear un objeto que permita fabricar las sesiones
+        SessionFactory factory = new Configuration().configure("cfg.xml").addAnnotatedClass(Mascota.class).buildSessionFactory();
+
+        //Fabricar sesión
+        Session session = factory.openSession();
+
+        //Preparar sesion para transacciones
+        session.beginTransaction();
+        try{
+            //Crear objeto
+            Mascota objMascota= new Mascota("Roko", "Acuna", "Criollo", "#", "Ninguna");
+            session.persist(objMascota);
+            session.getTransaction().commit();;
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        //cerrar sesión
+        session.close();
     }
 }
